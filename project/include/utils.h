7e7adeb731144d058507b7aa486fcf8423d8c459
tomanosenko <toma.nosenko@gmail.com>
2022-04-06 12:56:52 +0300

    commit

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,5 +1,5 @@
-#ifndef GITHUB_GIT_TEST_HW_PROJECT_INCLUDE_UTILS_H_
-#define GITHUB_GIT_TEST_HW_PROJECT_INCLUDE_UTILS_H_
+#ifndef PROJECT_INCLUDE_UTILS_H_
+#define PROJECT_INCLUDE_UTILS_H_
 
 #include <stdio.h>
 #include "data_struct.h"
@@ -12,4 +12,4 @@ void personal_data_write(FILE *of_p_inf);
 void transaction_write(FILE *of_p_inf);
 void update_transaction_data(FILE *of_p_inf, FILE  *of_transac, FILE *of_n_transac);
 
-#endif  //  GITHUB_GIT_TEST_HW_PROJECT_INCLUDE_UTILS_H_
+#endif  //  PROJECT_INCLUDE_UTILS_H_
